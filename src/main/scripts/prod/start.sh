ec2c668b15ce1ab2f2195f2b165f5f284ed40312
HOZONGMAN\hozongman <960229162@qq.com>
2018-11-05 15:19:19 +0800

    update 4

--- src/main/scripts/prod/start.sh
+++ src/main/scripts/prod/start.sh
@@ -8,6 +8,7 @@ LIB_DIR=$PROJECT_DIR/lib
 LIB_JARS=`ls $LIB_DIR | grep .jar | awk '{print "'$LIB_DIR'/"$0}' | tr "\n" ":"`
 mkdir -p /logs/$SERVICE_NAME/
 MAIN_CLASS="com.miles.demoservice.Application"
+JAVE_HOME=/usr/local/java
 
 # 设置classpath
-nohup java -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS | tee -a /data/app/build/$SERVICE_NAME/std_out.log 2>&1 &
+nohup ${JAVE_HOME}/jre/bin/java -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS | tee -a /data/app/build/$SERVICE_NAME/std_out.log 2>&1 &
